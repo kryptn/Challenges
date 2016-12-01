1b3e8684d351ef046bd3810de0be294a9536b6ff
David Bibb <kryptn@gmail.com>
2016-12-01 02:02:04 -0500

    Added second star

--- one.py
+++ one.py
@@ -10,20 +10,29 @@ class Dirs:
 
 class Grid:
 
+    #             west     north   east    south
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
 
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.dir = 1
+        self.dir = 1 # Assume initial direction north
+        self.visited = [(0,0)] # we start at 0,0
+        self.visited_twice = []
 
     def move(self, dir):
         turn = 1 if dir.direction  == 'R' else -1
         self.dir = (self.dir + turn) % 4
         vx, vy = self.directions[self.dir]
-        self.x += vx * dir.distance
-        self.y += vy * dir.distance
+
+        for x in range(dir.distance):
+            self.x += vx
+            self.y += vy
+            pos = (self.x, self.y)
+            self.visited.append(pos)
+
+            if self.visited.count(pos) == 2:
+                self.visited_twice.append(pos)
 
 grid = Grid()
 dirs = [Dirs(x) for x in path.split(', ')]
@@ -31,4 +40,5 @@ dirs = [Dirs(x) for x in path.split(', ')]
 for d in dirs:
     grid.move(d)
 
-print(abs(grid.x) + abs(grid.y))
+print('star one: ', abs(grid.x) + abs(grid.y))
+print('star two: ', sum(abs(x) for x in grid.visited_twice[0]))
