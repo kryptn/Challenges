e2fed6a4acda600f4c69c1ed4f531f55340188b3
David Bibb <kryptn@gmail.com>
2016-12-04 15:27:12 -0500

    Day four star two

--- four.py
+++ four.py
@@ -1,10 +1,19 @@
 
-from collections import Counter
+from collections import Counter, namedtuple
+from string import ascii_lowercase as alpha
 
 with open('data/four/input.txt') as fd:
     data = fd.read().splitlines()
 
 test = 'aaaaa-bbb-z-y-x-123[abxyz]'
+
+Room = namedtuple('Room', ('name', 'sector'))
+
+def word_shift(word, offset):
+    return ''.join( alpha[ (alpha.index(c)+offset)%26 ] for c in word)
+
+def shift(words, offset):
+    return ' '.join(word_shift(word, offset) for word in words)
 
 def truth(enc):
     elements = enc.split('-')
@@ -12,11 +21,18 @@ def truth(enc):
     counter = Counter(''.join(elements[:-1]))
     tcs = sorted( sorted(counter.keys()), key=lambda x: counter[x], reverse=True)
     if checksum == ''.join(tcs[:5]):
-        return int(sector)
-    return 0
+        return Room(shift(elements[:-1], int(sector)), int(sector))
+    return None
 
-true_data = [x for x in data if truth(x)]
 
-print('star one: {} '.format(sum(truth(x) for x in true_data))
+# filter out fake rooms
+true_data = list(filter(lambda x: x, [truth(x) for x in data]))
+
+# sum of real room sectors
+print('star one: {}'.format( sum(x.sector for x in true_data) ))
+
+# find north pole object storage
+print('star two: {}'.format( list(filter(lambda x: 'northpole' in x.name, true_data))[0].sector))
+
 
 
