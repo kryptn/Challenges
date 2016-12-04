ecc9d3a53aa968b7ec8af0550e9041e5062d6a74
David Bibb <kryptn@gmail.com>
2016-12-04 14:32:34 -0500

    Added day 3 part 2

--- three.py
+++ three.py
@@ -1,5 +1,9 @@
+from itertools import izip_longest
+
 with open('data/three/input.txt') as fd:
-    data = [[int(x) for x in sorted(y.split())] for y in fd.read().splitlines()]
+    data = [[int(x) for x in y.split()] for y in fd.read().splitlines()]
+
+
 
 def check(a, b, c):
     if a+b > c:
@@ -8,5 +12,13 @@ def check(a, b, c):
                 return True
     return False
 
+def twisted(d):
+    args = [iter(d)] * 3
+    nd = list(izip_longest(*args))
+    zd = list(zip(*x) for x in nd)
+    return [x for y in zd for x in y]
+
 
 print(len([x for x in data if check(*x)]))
+print(len([x for x in twisted(data) if check(*x)]))
+
