948c82fc5c151b180b211728fa900776ec2e5a70
David Bibb <kryptn@gmail.com>
2016-12-05 16:00:53 -0500

    Abstracted all the things

--- 2016/day_05/five.py
+++ 2016/day_05/five.py
@@ -1,12 +1,6 @@
 from hashlib import md5
 from itertools import count
 
-
-#with open('input.txt') as fd:
-#    data = fd.read()
-
-data = 'ojvtpuvg'
-
 def check(code):
     hashed = md5(code).hexdigest()
 
@@ -14,59 +8,36 @@ def check(code):
         return hashed[5], hashed[6]
     return None
 
-def crack_basic(seed):
-    counter = count()
-    password = ''
-    while len(password) < 8:
-        r = check(seed+str(counter.next()))
-        if not r:
-            continue
-        password += r[0]
-
+def basic(hashed, password):
+    password += hashed[0]
     return password
 
-def crack_adv(seed):
+def adv(hashed, password):
+    pos, c = hashed
+    if pos.isdigit() and int(pos) < 8 and pos not in password:
+        password[pos] = c
+    return password
+
+def finish(pw):
+    return ''.join(v for k,v in sorted(pw.items(), key=lambda x: x[0]))
+
+def crack(seed, method, pass_obj, handler=None):
     counter = count()
-    password = {}
-    while len(password) < 8:
-        r = check(seed+str(counter.next()))
-        if not r:
-            continue
-        pos, c = r
-        if pos.isdigit() and int(pos) < 8 and pos not in password:
-            password[pos] = c
-
-    return ''.join(v for k,v in sorted(password.items(), key=lambda x: x[0]))
-
-
-
-
-def v2(seed):
-
-    password = {}
-    def check(inp):
-        h = md5(inp).hexdigest()
-        if h.startswith('00000'):
-            print(inp)
-            pos = h[5]
-            c = h[6]
-            if pos.isdigit() and int(pos) < 8:
-                print('\t Passed')
-                return pos, c
-            print('\t Failed')
-        return None
-    counter = count(0)
+    password = pass_obj()
 
     while len(password) < 8:
         r = check(seed+str(counter.next()))
         if r:
-            pos, c = r
-            if pos not in password:
-                password[pos] = c
+            password = method(r, password)
 
-    print(''.join(v for k,v in sorted(password.items(), key=lambda x: x[0])))
+    if handler:
+        return handler(password)
 
-# v1(data)
+    return password
 
-# v2(data)
+data = 'ojvtpuvg'
+
+# crack(data, basic, str)
+
+# crack(data, adv, dict, finish)
 
