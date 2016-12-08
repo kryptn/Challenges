5322c7a7e280196e39f21ad3c396f470f0cd78bc
kryptn <kryptn@gmail.com>
2016-12-07 22:20:37 -0800

    Day seven star two

--- 2016/day_07/seven.py
+++ 2016/day_07/seven.py
@@ -16,13 +16,25 @@ def parse(ips):
     split_ip = ips.replace('[', ' ').replace(']', ' ').split()
     return split_ip[::2], split_ip[1:][::2]
 
-def verify_ip(ip):
+def verify_TLS(ip):
     n, h = parse(ip)
     if any(any(abba(x) for x in chunks(y, 4)) for y in n):
         if not any(any(abba(x) for x in chunks(y, 4)) for y in h):
             return True
     return False
 
-r = [verify_ip(x) for x in data]
+def verify_SSL(ip):
+    supernet, hypernet = parse(ip)
+    flip = lambda s: s[1]+s[0]+s[1]
+    superchunks = [x for y in [chunks(z, 3) for z in supernet] for x in y]
+    hyperchunks = [x for y in [chunks(z, 3) for z in hypernet] for x in y]
 
-print('star one: {}'.format(sum(r)))
+    abas = [chunk for chunk in superchunks if abba(chunk)]
+
+    return set([flip(x) for x in abas]).intersection(set(hyperchunks))
+
+tls = [verify_TLS(x) for x in data]
+ssl = [True if verify_SSL(x) else False for x in data]
+
+print('star one: {}'.format(sum(tls)))
+print('star two: {}'.format(sum(ssl)))
