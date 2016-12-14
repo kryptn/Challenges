da4f100f340f1e939001b6ad4d3e07ea86a5e1c1
kryptn <kryptn@gmail.com>
2016-12-14 00:02:38 -0800

    Completely redid it. part two took an hour to run initially

--- 2016/day_14/fourteen.py
+++ 2016/day_14/fourteen.py
@@ -1,42 +1,63 @@
 from hashlib import md5
 from itertools import count
+from collections import deque, namedtuple
 import re
 
 pat = re.compile(r'(.)\1\1')
 
 data = 'cuanljph'
 
-def mhash(d, i, stretch=False):
-    if stretch:
-        f = mhash(d, i)
-        for x in xrange(2016):
-            f = md5(f).hexdigest()
-        return f
-    return md5(d+str(i)).hexdigest()
+IndexedHash = namedtuple('IndexedHash', ('index', 'hash'))
 
-def otp(seed, stretch=False):
-    counter = count()
-    hashes = []
+class Hashes:
+
     pattern = re.compile(r'(.)\1\1')
+    roll = 2016
 
-    c = counter.next()
-    while len(hashes) < 64:
-        result = pattern.findall(mhash(seed, c, stretch))
-        if result:
-            print('hash found? {}'.format(c))
-            for x in xrange(c+1, c+1000):
-                if result[0]*5 in mhash(data, x, stretch):
-                    print('\t\t\tFound!')
-                    hashes.append(c)
-        c = counter.next()
+    def __init__(self, seed, super_secure=False):
+        self.seed = seed
+        self.super_secure = super_secure
+        self.hashes = deque()
+        self.results = []
+        self.init_hashes()
 
-    return hashes
+        self.run()
 
-r = otp(data)
+    def hack(self, index):
+        h = md5(self.seed+str(index)).hexdigest()
+        if self.super_secure:
+            for _ in xrange(self.roll):
+                h = md5(h).hexdigest()
+        self.hashes.append(IndexedHash(index, h))
 
-print(r[-1])
-            
-r = otp(data, True)
+    def init_hashes(self):
+        for index in xrange(1000):
+            self.hack(index)
+        self.latest = index
 
-print(r[-1])
+    def cycle(self):
+        self.hack(self.hashes[-1].index+1)
+        return self.hashes.popleft()
+
+    def check(self):
+        h = self.cycle()
+        r = self.pattern.findall(h.hash)
+        if r:
+            if any(r[0]*5 in x.hash for x in self.hashes):
+                self.results.append(h)
+
+    def run(self):
+
+        while len(self.results) < 64:
+            self.check()
+
+        print(self.hashes[-1].index)
+
+h = Hashes(data)
+hs = Hashes(data, True)
+
+
+
+
+
 
