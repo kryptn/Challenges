fe66c9154a2346b48440053f8e600bbcd2302c0c
kryptn <kryptn@gmail.com>
2016-12-19 00:46:13 -0700

    day 19 star 2

--- 2016/day_19/nineteen.py
+++ 2016/day_19/nineteen.py
@@ -1,3 +1,5 @@
+from time import time
+
 data = 3017957
 
 def josephus(n):
@@ -6,4 +8,32 @@ def josephus(n):
 
 print(josephus(data))
 
+class Item:
+    def __init__(self, pos):
+        self.pos = pos
+        self.n = None
+        self.p = None
 
+    def steal(self):
+        self.p.n = self.n
+        self.n.p = self.p
+
+def eliminate(n):
+    circle = [Item(x) for x in xrange(n)]
+    for elf in xrange(n):
+        circle[elf].n = circle[(elf+1)%n]
+        circle[elf].p = circle[(elf-1)%n]
+
+    current = circle[0]
+    mid = circle[n/2]
+
+    for elf in xrange(n-1):
+        mid.steal()
+        mid = mid.n
+        if (n-elf)%2 == 1:
+            mid = mid.n
+        current = current.n
+
+    return current.pos+1
+
+eliminate(data)
