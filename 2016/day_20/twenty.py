7397e2e31eb0938c30ababf55ba2cbbb42d3a61a
kryptn <kryptn@gmail.com>
2016-12-19 22:38:10 -0700

    Cleaned up

--- 2016/day_20/twenty.py
+++ 2016/day_20/twenty.py
@@ -6,9 +6,9 @@ def test_ip(n):
         if start <= n <= end:
             break
     else:
-        return True
+        if n < 2**32:
+            return True
     return False
-
 
 data = sorted([int(x), int(y)] for x,y in [z.split('-') for z in data.splitlines()])
 
@@ -18,13 +18,10 @@ valids = [c for c in candidates if test_ip(c)]
 
 total = 0
 for ip in valids:
-    repeat = True
-    while repeat:
-        if test_ip(ip) and ip < 4294967296:
-            total += 1
-            ip += 1
-        else:
-            repeat = False
+    while test_ip(ip):
+        total += 1
+        ip += 1
+
 
 print(valids[0])
 print(total)
